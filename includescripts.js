c5ba291ce9ab66e1f2cfa4d90c502362e2d6abd1
Daz Azemoon <daz.azemoon@googlemail.com>
2015-03-23 17:06:04 +0000

    Update includescripts.js

--- includescripts.js
+++ includescripts.js
@@ -21,23 +21,33 @@
 
     includeScripts() loads and executes scripts if not already loaded
 
-        A check is performed to see if the file is loaded as a script already
-        if not a script node is created dangling off the same parent node as the currently executing script
-        and its onload function is constructed to pass on the current executing scripts onload function
-        and some payload function which is the body of the dependant script.
+    A check is performed to see if the file is loaded as a script already
+    if not a script node is created dangling off the same parent node as the currently executing script
+    and its onload function is constructed to pass on the current executing scripts onload function 
+    and some payload function which is the body of the dependant script.
 
-        It doesn't matter whether the node is loaded synchronously or asynchronously; either way the files
-        are each loaded once, one at a time, in order of dependency, and are visible to debuggers as if they were
-        loaded statically.
+    It doesn't matter whether the node is loaded synchronously or asynchronously; either way the files
+    are each loaded once, one at a time, in order of dependency, and are visible to debuggers as if they were
+    loaded statically.
 
-        USAGE:    includeScripts([<file>,<file>,..,<file>], function () {
-                    // do stuff that depends on included files
-                },
-                function (filename) {
-                    // do stuff to report that file <filename> wasn't loaded
-                });
+    Furthermore, any file included this way can itself use includeScripts to include further files (thus 
+    implementing the acyclic directed graph of file dependancies)
 
-        NOTE:     external css files can also be included(this injects a link node into the dom)
+    USAGE:  includeScripts([<file>,<file>,..,<file>], function () {
+                // do stuff that depends on included files
+            },
+            function (filename) {
+                // (optional) error handler - do stuff to report that file <filename> wasn't loaded 
+            });
+
+    CAVEATS:  - All files in the dependancy graph are loaded synchronously (in series) so browsers won't leverage
+                multi-core concurrent loading.
+              - Each file in the dependancy graph makes a hit on its hosting server
+
+    NOTE:     - Code executed in the 'do stuff' is guaranteed to run AFTER all dependancies are loaded
+              - Assumes scripts are running on a browser (modifies the DOM)
+              - CSS files can also be included (injects link nodes into the DOM)
+              - Any file that appears as script can be included (injects script nodes into the DOM)
 */
 
 /*jslint browser: true, regexp: true, vars: true, plusplus: true*/
